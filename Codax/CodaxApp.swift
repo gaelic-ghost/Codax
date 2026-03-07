3127940f5d55d440f74c781c9114a21c04fbb75f
Gale W <mail@galewilliams.com>
2026-03-07 16:05:17 -0500

    Initial Commit

---
+++ Codax/CodaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodaxApp.swift
+//  Codax
+//
+//  Created by Gale Williams on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CodaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
